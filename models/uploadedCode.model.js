4630fdc8ddaec98054e639b390bde481a1dcb5d5
harimancodes <iamsreeharipremkumar@gmail.com>
2022-06-30 23:08:08 +0530

    sree_1.9

--- models/uploadedCode.model.js
+++ models/uploadedCode.model.js
@@ -3,7 +3,6 @@ const bcrypt = require('bcrypt');
 const createHttpError = require('http-errors');
 
 const UploadedCodeSchema = new mongoose.Schema({
-
   email: {
     type: String,
     required: true,
@@ -46,29 +45,5 @@ const UploadedCodeSchema = new mongoose.Schema({
   },
 });
 
-UserSchema.pre('save', async function (next) {
-  try {
-    if (this.isNew) {
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(this.password, salt);
-      this.password = hashedPassword;
-      if (this.email === process.env.ADMIN_EMAIL.toLowerCase()) {
-        this.role = roles.admin;
-      }
-    }
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
-
-UserSchema.methods.isValidPassword = async function (password) {
-  try {
-    return await bcrypt.compare(password, this.password);
-  } catch (error) {
-    throw createHttpError.InternalServerError(error.message);
-  }
-};
-
-const uploadedCode = mongoose.model('uploadedCode',UploadedCodeSchema);
-module.exports = uploadedCode;
+const uploadedCodeModel = mongoose.model('uploadedCode',UploadedCodeSchema);
+module.exports = uploadedCodeModel;
--- routes/student.route.js
+++ routes/student.route.js
@@ -3,47 +3,106 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const request = require("request");
+const uploadedCodeModel = require("../models/UploadedCode.model");
+const axios = require('axios').default;
 
-router.get("",(req,res,next)=>{
-  res.render("studentHome")
-})
+router.get("", (req, res, next) => {
+  res.render("studentHome");
+});
 router.get("/upload-code", (req, res, next) => {
   res.render("uploadCode");
 });
 router.post("/uploadCodeFile", (req, res, next) => {
   var code = req.body.code;
   //code=code.replace(/(\r\n|\n|\r)/gm,"");
-
-  var program = {
-    script: code,
-    language: "c",
-    stdin:"3 5",
-    versionIndex: "0",
-    clientId: "10b08afe3f797dcd5f7df7af153f8b01",
-    clientSecret:
-      "383e2dfeefc95027783c6ec540d551b031d685d7f574132c110025efec5a407b",
-  };
-
-  console.log(program);
-
-  request(
-    {
-      url: "https://api.jdoodle.com/v1/execute",
-      method: "POST",
-      json: program,
-    },
-    function (error, response, body) {
-      console.log("Error:", error);
-      console.log("StatusCode:", response && response.statusCode);
-      console.log("Body:", body);
+  const ioMap = new Map([
+    ["5 3", "8"],
+    ["4 2", "6"],
+    ["8 6", "14"],
+  ]);
+  var testCasePassed = [0,0,0];
+  var i = 0;
+  ioMap.forEach(function (value, key) {
+    var program = {
+      script: code,
+      language: "c",
+      stdin: key,
+      versionIndex: "0",
+      clientId: "10b08afe3f797dcd5f7df7af153f8b01",
+      clientSecret:
+        "383e2dfeefc95027783c6ec540d551b031d685d7f574132c110025efec5a407b",
+    };
+    
+    axios.post('https://api.jdoodle.com/v1/execute',program).then(function (response){
+      console.log(response.data['output']);
+      if (response.data['output'] == value) {
+        console.log("output : ",response.data['output']);
+        console.log("Value : ",value);
+       testCasePassed[i] = 1;
+      }else 
+      testCasePassed[i]=0;
+      i++;
+      const tMark=new Map([
+        [0,"\u274c"],
+        [1,"\u2705"],
+      ])
+      if(i==3){
+      var testCaseResult = {
+        t1: tMark.get(testCasePassed[0]),
+        t2: tMark.get(testCasePassed[1]),
+        t3: tMark.get(testCasePassed[2]),
+      };
+      
+      console.log(testCaseResult);
+      res.render("uploadCode", { testCaseResult: testCaseResult });
     }
-  );
-  const testCaseResult = {
-    t1: '\u274C',
-    t2: '\u2705',
-    t3: '\u274C'
-}
+    }).catch(function (error) {
+      console.log(error);
+    });
+    
+    
+        
+      }
+     );
+  });
+
+
   
-  res.render("uploadCode",{testCaseResult:testCaseResult});
-});
+
+  //uploading code to database
+
+  // var uploadedCodeData = new uploadedCodeModel({
+  //   email: "19cs201@mgist.ac.in",
+  //   courseCode: "CST-301",
+  //   code: code,
+  //   language: "c",
+  //   experimentNumber: "1",
+  //   batchFrom: "2019",
+  //   batchTo: "2023",
+  //   className: "CSE",
+  // });
+
+  // uploadedCodeData.save((err, doc) => {
+  //   if (!err) {
+  //     req.flash("success", "Code uploaded successfully!");
+  //   } else console.log("Error during record insertion : " + err);
+  // });
+
+  
+
 module.exports = router;
+
+//Trial Code
+/*#include <stdio.h>
+int main() {    
+
+    int number1, number2, sum;
+   
+    scanf("%d %d", &number1, &number2);
+
+    // calculating sum
+    sum = number1 + number2;      
+    
+    printf("%d",sum);
+    return 0;
+}*/
